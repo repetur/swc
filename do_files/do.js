9f5b639736f4ece58ba844f352489e40c869f059
repetur <66164138+repetur@users.noreply.github.com>
2023-05-11 12:19:51 +0300

    init

---
+++ do_files/do.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0 local-build-20230511
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('do_files/doMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
